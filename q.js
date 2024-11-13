256984462ae84188b18c6dd5fdc72d041530daf7
Jared Akigbesote <9bn46t32@students.codeinstitute.net>
2024-11-13 13:01:15 +0000

    add quiz.html and q.js

---
+++ q.js
@@ -0,0 +1,241 @@
+{
+    question: "Which movie is the higest grossing of all time?",
+    answers: [{
+            text: "Star Wars: The Force Awakens",
+            correct: false
+        },
+        {
+            text: "Titanic",
+            correct: false
+        },
+        {
+            text: "The Lion King",
+            correct: false
+        },
+        {
+            text: "Avatar",
+            correct: true
+        },
+        {
+            text: "Avengers: Endgame",
+            correct: false
+        },
+    ]
+},{{
+    question: "In what year did the Russian revolution end?",
+    answers: [{
+            text: "1923",
+            correct: true
+        },
+        {
+            text: "1917",
+            correct: false
+        },
+        {
+            text: "1918",
+            correct: false
+        },
+        {
+            text: "1921",
+            correct: false
+        },
+        {
+            text: "1914",
+            correct: false
+        },
+    ]
+},
+{
+    question: "What is the closest star system to ours?",
+    answers: [{
+            text: "Epsilon Indi",
+            correct: false
+        },
+        {
+            text: "Barnard's Star",
+            correct: false
+        },
+        {
+            text: "Alpha Canis Majoris",
+            correct: false
+        },
+        {
+            text: "Luhman 16",
+            correct: false
+        },
+        {
+            text: "Alpha Centauri",
+            correct: true
+        },
+    ]
+
+},
+{
+    question: "What country is known as the land of the rising sun?",
+    answers: [{
+            text: "South Africa",
+            correct: false
+        },
+        {
+            text: "Australia",
+            correct: false
+        },
+        {
+            text: "Cuba",
+            correct: false
+        },
+        {
+            text: "Japan",
+            correct: true
+        },
+        {
+            text: "England",
+            correct: false
+        },
+    ]
+
+},
+{
+    question: "Which athlete holds the most olympic medals?",
+    answers: [{
+            text: "Usain Bolt",
+            correct: false
+        },
+        {
+            text: "Larisa Latynina",
+            correct: false
+        },
+        {
+            text: "Michael Phelps",
+            correct: true
+        },
+        {
+            text: "Mo Farah",
+            correct: false
+        },
+        {
+            text: "Katō Sawao",
+            correct: false
+        },
+    ]
+},
+{
+    question: "Which car manufacturer built the first supercar?",
+    answers: [{
+            text: "Porsche 911",
+            correct: false
+        },
+        {
+            text: "Lamborghini",
+            correct: true
+        },
+        {
+            text: "Aston Martin",
+            correct: false
+        },
+        {
+            text: "Ferrari",
+            correct: false
+        },
+        {
+            text: "Ford",
+            correct: false
+        },
+    ]
+},
+{
+    question: "What year did the nintendo ds come out?",
+    answers: [{
+            text: "2000",
+            correct: false
+        },
+        {
+            text: "2003",
+            correct: false
+        },
+        {
+            text: "2001",
+            correct: false
+        },
+        {
+            text: "2006",
+            correct: false
+        },
+        {
+            text: "2004",
+            correct: true
+        },
+    ]
+},
+{
+    question: "Which kingdom united and formed the German Empire?",
+    answers: [{
+            text: "East Frankish kingdom,",
+            correct: false
+        },
+        {
+            text: "Kingdom of Bavaria",
+            correct: false
+        },
+        {
+            text: "Kingdom of Prussia",
+            correct: true
+        },
+        {
+            text: "Kingdom of Württemberg",
+            correct: false
+        },
+        {
+            text: "Kingdom of Saxony",
+            correct: false
+        },
+    ]
+},
+{
+    question: "which planet has the most moons?",
+    answers: [{
+            text: "Uranus",
+            correct: false
+        },
+        {
+            text: "Jupiter",
+            correct: false
+        },
+        {
+            text: "Saturn",
+            correct: true
+        },
+        {
+            text: "Neptune",
+            correct: false
+        },
+        {
+            text: "Mars",
+            correct: false
+        },
+    ]
+},
+{
+    question: "What is the capital city of Finland",
+    answers: [{
+            text: "Helsinki",
+            correct: true
+        },
+        {
+            text: "Oslo",
+            correct: false
+        },
+        {
+            text: "Tallinn",
+            correct: false
+        },
+        {
+            text: "Tampere",
+            correct: false
+        },
+        {
+            text: "Oulu",
+            correct: false
+        },
+    ]
+},
